5821d4bf19709105c26672eeffafb85aaa82877e
Mikkel Pedersen <mikkel.pedersen96@live.dk>
2021-02-11 14:37:54 +0100

    Only resize selected

--- src/app/designpad/toolings/resize-state-tool.ts
+++ src/app/designpad/toolings/resize-state-tool.ts
@@ -1,17 +1,18 @@
 import {Tool} from "./tool";
 import {ToolTypes} from "./toolTypes";
 import {Triple} from "../elements/triple";
-import {implementsClickable, implementsDraggable, implementsOnReleased} from "../implements";
+import {implementsClickable, implementsSelectable} from "../implements";
 import {Point} from "../elements/point";
 import {BasicState} from "../elements/basicState";
-import {EventDispatcher, EventType} from "../eventDispatcher";
 
 export class ResizeStateTool extends Tool {
   readonly name: ToolTypes = 'ResizeStateTool';
   private cameraOffset: Point;
+  private selected: BasicState;
 
   public mouseClicked(elements: any[], cameraPosition: Triple, mouseCoordinates: Point): void {
-    const test = elements.filter((e) => implementsClickable(e)).forEach(e => e.clickEvent(cameraPosition));
+    elements.filter((e) => implementsClickable(e)).forEach(e => e.clickEvent(cameraPosition));
+    this.selected = elements.filter((e) => implementsSelectable(e)).find(e => e.selectEvent(cameraPosition));
     this.cameraOffset = {
       x: (cameraPosition.x * cameraPosition.z) - mouseCoordinates.x,
       y: (cameraPosition.y * cameraPosition.z) - mouseCoordinates.y
@@ -19,10 +20,9 @@ export class ResizeStateTool extends Tool {
   }
 
   public mouseDragged(elements: any[], cameraPosition: Triple, mouseCoordinates: Point): void {
-    const x = (mouseCoordinates.x + this.cameraOffset.x) / cameraPosition.z;
-    const test: BasicState = elements[1];
-    if (x > 0) {
-      test.resize(x);
+    const scale = (mouseCoordinates.x + this.cameraOffset.x) / cameraPosition.z;
+    if (scale > 0) {
+      this.selected.resize(scale);
     }
   }
 }
