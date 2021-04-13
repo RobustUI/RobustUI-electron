2f60abeb9e06f664e6c0fd33839ba624ff44e91c
Mikkel Pedersen <mikkel.pedersen96@live.dk>
2021-04-13 12:47:01 +0200

    Notify user if unused actions (#31)
    * It works
    
    * Made better solution
    
    * Fixed small thing
    
    * Added alert
    
    * Little clean up

--- src/app/designpad/designpad/designpad.component.ts
+++ src/app/designpad/designpad/designpad.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {RobustUiComponent} from "../../entities/robust-ui-component";
 import {Subject} from "rxjs";
-import {EventDispatcher, EventType} from "../eventDispatcher";
+import {Event, EventDispatcher, EventType} from "../eventDispatcher";
 import {ToolTypes} from "../toolings/toolTypes";
 import {PadControllerComponent} from "../pad-controller/pad-controller.component";
 import {SimulatorTrace} from "../../interfaces/simulator-trace";
@@ -9,6 +9,7 @@ import {RobustUiStateTypes} from "../../entities/robust-ui-state-types";
 import {RobustUiSimpleComponent} from "../../entities/robust-ui-simple-component";
 import {DesignPadToRobustUi} from "../converters/DesignPadToRobustUi";
 import {RobustUiState} from "../../entities/robust-ui-state";
+import {RobustUiSelectiveComponent} from "../../entities/robust-ui-selective-component";
 
 export interface UpdateComponent {
   newLabel: string;
@@ -25,6 +26,7 @@ export class DesignpadComponent implements OnInit {
   public set component(value: RobustUiComponent) {
     this.activeComponent = value;
     this.tempComponentLabel = value.label;
+    this.findUsedActions();
   }
 
   @ViewChild(PadControllerComponent) padController: PadControllerComponent;
@@ -41,6 +43,8 @@ export class DesignpadComponent implements OnInit {
   public activeTool: ToolTypes = 'SelectTool';
   public selectedRenameAction = "";
   public tempActionName = "";
+  public usedInput: Set<string> = new Set<string>();
+  public usedOutput: Set<string> = new Set<string>();
 
   constructor() {
   }
@@ -51,11 +55,26 @@ export class DesignpadComponent implements OnInit {
         (this.activeComponent as RobustUiSimpleComponent).states.set(newComp.label, newComp);
       }
     });
+
     this.listenForComponentChanges();
   }
 
   public activateTool(toolName: ToolTypes): void {
     this.activeTool = toolName;
+  }
+
+  public findUsedActions(updatedComponent = null): void {
+    this.clearUsedAction();
+
+    switch (this.activeComponent.type) {
+      case RobustUiStateTypes.simpleComponent:
+        this.findUsedActionForSimpleComponent(updatedComponent);
+        break;
+      case RobustUiStateTypes.compositeComponent:
+      case RobustUiStateTypes.selectiveComponent:
+        this.findUsedActionForSelectiveComponent();
+        break;
+    }
   }
 
   public addNewInput(input: string): void {
@@ -73,6 +92,11 @@ export class DesignpadComponent implements OnInit {
   }
 
   public save(): void {
+    const amountOfActionForComponent = this.activeComponent.outputs.size + this.activeComponent.inputs.size;
+    const amountOfUsedActionsForComponent = this.usedInput.size + this.usedOutput.size;
+    if (amountOfActionForComponent !== amountOfUsedActionsForComponent) {
+      alert("You have some inputs or outputs that you do not use");
+    }
     EventDispatcher.getInstance().emit({type: EventType.SAVE_COMPONENT, data: this.activeComponent});
   }
 
@@ -147,20 +171,54 @@ export class DesignpadComponent implements OnInit {
     }
   }
 
+  private findUsedActionForSelectiveComponent() {
+    const castedComponent = this.activeComponent as RobustUiSelectiveComponent;
+    this.usedInput.add(castedComponent.observer.input);
+  }
+
+  private findUsedActionForSimpleComponent(updatedComponent = null) {
+    let castedComponent;
+    if (updatedComponent != null) {
+      castedComponent = updatedComponent;
+    } else {
+      castedComponent = this.activeComponent as RobustUiSimpleComponent;
+    }
+
+    castedComponent.transitions.forEach(e => {
+      if (castedComponent.inputs.has(e.label)) {
+        this.usedInput.add(e.label);
+      } else if (castedComponent.outputs.has(e.label)) {
+        this.usedOutput.add(e.label);
+      }
+    });
+  }
+
   private listenForComponentChanges() {
     EventDispatcher.getInstance().stream().subscribe(event => {
       if (event.type === EventType.CHANGE_COMPONENT) {
         this.activeComponent = DesignPadToRobustUi.convert(event.data, this.activeComponent);
       } else if (event.type === EventType.RENAME_STATE) {
-        const state = event.data.newState as RobustUiState;
-        const component = this.activeComponent as RobustUiSimpleComponent;
-        component.states.set(event.data.previousName, state);
-        if (event.data.previousName === component.initialState.label) {
-          component.initialState = state;
-        }
-        this.activeComponent = component;
-        this.save();
+        this.updateStateName(event);
+      } else if (event.type === EventType.RENAME_ACTION) {
+        this.usedInput.delete(event.data.prev.slice(0, -1));
+        this.usedInput.add(event.data.new.slice(0, -1));
       }
     });
+  }
+
+  private updateStateName(event: Event) {
+    const state = event.data.newState as RobustUiState;
+    const component = this.activeComponent as RobustUiSimpleComponent;
+    component.states.set(event.data.previousName, state);
+    if (event.data.previousName === component.initialState.label) {
+      component.initialState = state;
+    }
+    this.activeComponent = component;
+    this.save();
+  }
+
+  private clearUsedAction() {
+    this.usedInput.clear();
+    this.usedOutput.clear();
   }
 }
